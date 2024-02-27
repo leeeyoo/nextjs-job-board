a2ae597e430d2b8006b5bb5207bd72259bc91834
leeeyoo <leeeyoo@gmail.com>
2024-02-27 17:42:19 +0900

    feat: job filter sidebar

---
+++ src/lib/job-types.ts
@@ -0,0 +1,10 @@
+export const jobTypes = [
+  "Full-time",
+  "Part-time",
+  "Contract",
+  "Temporaty",
+  "Internship",
+  "Volunteer",
+];
+
+export const locationTypes = ["Remote", "On-site", "Hybrid"];
