ff72930462bba2a0550574e8c6d2175988a0cd39
leeeyoo <leeeyoo@gmail.com>
2024-03-15 18:59:39 +0900

    feat: job details page

--- src/components/JobResults.tsx
+++ src/components/JobResults.tsx
@@ -1,10 +1,11 @@
-import { JobFilerValues } from "@/lib/validation";
+import { JobFilterValues } from "@/lib/validation";
 import JobListItem from "./JobListItem";
 import prisma from "@/lib/prisma";
 import { Prisma } from "@prisma/client";
+import Link from "next/link";
 
 interface JobResultsProps {
-  filterValues: JobFilerValues;
+  filterValues: JobFilterValues;
 }
 
 export default async function JobResults({
@@ -45,7 +46,9 @@ export default async function JobResults({
   return (
     <div className="grow space-y-4">
       {jobs.map((job) => (
-        <JobListItem job={job} key={job.id} />
+        <Link key={job.id} href={`/jobs/${job.slug}`} className="block">
+          <JobListItem job={job} />
+        </Link>
       ))}
       {jobs.length === 0 && (
         <p className="m-auto text-center">
